b72446688a837f37715b6960d74f019b4611f47b
videv <vtounaregha@gmail.com>
2025-02-21 12:41:55 +0100

    displays the score and result in the DOm

--- rock-paper-scissors/js/script.js
+++ rock-paper-scissors/js/script.js
@@ -5,10 +5,21 @@ let score = {
     totalPlayed: 0
 }
 
+const displayResult = document.querySelector('.results');
+const displayScore = document.querySelector('.scores');
+const resetBtn = document.querySelector('#reset');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
 score = JSON.parse(localStorage.getItem('score'));
+
+
 
 let reset = () => {
     if (score.totalPlayed === 5 && score.humanScore > score.computerScore) {
+            displayResult.innerHTML = `Final result: <br> Your score: ${score.humanScore}, Computer Score: ${score.computerScore}. You won the game.`; 
+            displayScore.textContent = '';
             console.log(`Your score: ${score.humanScore}, Computer Score: ${score.computerScore}. You won the game.`);
             score = {
                 humanScore: 0,
@@ -17,6 +28,8 @@ let reset = () => {
                 totalPlayed: 0
             }
         } else if (score.totalPlayed === 5 && score.humanScore < score.computerScore) {
+            displayResult.innerHTML = `Final result: <br> Your Score: ${score.humanScore}, Computer Score: ${score.computerScore}. You lost the game. `;
+            displayScore.textContent = '';
             console.log(`Your Score: ${score.humanScore}, Computer Score: ${score.computerScore}. You lost the game. `);
             score = {
                 humanScore: 0,
@@ -25,6 +38,8 @@ let reset = () => {
                 totalPlayed: 0
             }
         } else if (score.totalPlayed === 5 && score.humanScore === score.computerScore) {
+            displayResult.innerHTML = `Final result: <br> Your Score: ${score.humanScore}, Computer Score: ${score.computerScore}. It's a tie.`;
+            displayScore.textContent = '';
             console.log(`Your Score: ${score.humanScore}, Computer Score: ${score.computerScore}. It's a tie.`);
             score = {
                 humanScore: 0,
@@ -69,6 +84,9 @@ let playGame = () => {
         if (humanChoice === computerChoice) {
             score.tie++;
             score.totalPlayed++;
+            displayScore.innerHTML = `Score: <br> Your Score: ${score.humanScore}. Computer Score: ${score.computerScore}.
+            Ties: ${score.tie}. Total Played: ${score.totalPlayed}`;
+            displayResult.textContent = `It's a tie! Computer picked ${computerChoice} and you picked ${humanChoice}`;
             return {
                 log: console.log(`It's a tie! Computer picked ${computerChoice} and you picked ${humanChoice}`),
                 score: console.log(score)
@@ -76,6 +94,9 @@ let playGame = () => {
         } else if (humanChoice === "rock" && computerChoice === "scissors") {
             score.humanScore++;
             score.totalPlayed++;
+            displayScore.innerHTML = `Score: <br> Your Score: ${score.humanScore}. Computer Score: ${score.computerScore}.
+            Ties: ${score.tie}. Total Played: ${score.totalPlayed}`;
+            displayResult.textContent = `You win! Computer picked ${computerChoice} and you picked ${humanChoice}. `;
             return {
                 log: console.log(`You win! Computer picked ${computerChoice} and you picked ${humanChoice}. `),
                 score: console.log(score)
@@ -83,6 +104,9 @@ let playGame = () => {
         } else if (humanChoice === "scissors" && computerChoice === "paper") {
             score.humanScore++;
             score.totalPlayed++;
+            displayScore.innerHTML = `Score: <br> Your Score: ${score.humanScore}. Computer Score: ${score.computerScore}.
+            Ties: ${score.tie}. Total Played: ${score.totalPlayed}`;
+            displayResult.textContent = `You win! Computer picked ${computerChoice} and you picked ${humanChoice}.`;
             return {
                 log: console.log(`You win! Computer picked ${computerChoice} and you picked ${humanChoice}.`),
                 score: console.log(score)
@@ -90,6 +114,9 @@ let playGame = () => {
         } else if (humanChoice === "paper" && computerChoice === "rock") {
             score.humanScore++;
             score.totalPlayed++;
+            displayScore.innerHTML = `Score: <br> Your Score: ${score.humanScore}. Computer Score: ${score.computerScore}.
+            Ties: ${score.tie}. Total Played: ${score.totalPlayed}`;
+            displayResult.textContent = `You win! Computer picked ${computerChoice} and you picked ${humanChoice}.`;
             return {
                 log: console.log(`You win! Computer picked ${computerChoice} and you picked ${humanChoice}.`),
                 score: console.log(score)
@@ -97,6 +124,9 @@ let playGame = () => {
         } else {
             score.computerScore++;
             score.totalPlayed++;
+            displayScore.innerHTML = `Score: <br> Your Score: ${score.humanScore}. Computer Score: ${score.computerScore}.
+            Ties: ${score.tie}. Total Played: ${score.totalPlayed}`;
+            displayResult.textContent = `You lose! Computer picked ${computerChoice} and you picked ${humanChoice}.`;
             return {
                 log: console.log(`You lose! Computer picked ${computerChoice} and you picked ${humanChoice}.`),
                 score: console.log(score)
@@ -110,7 +140,13 @@ let playGame = () => {
      playRound(humanSelection, computerSelection);
 }
 playGame();
-reset();
+//reset();
+/*score = {
+    humanScore: 0,
+    computerScore: 0,
+    tie: 0,
+    totalPlayed: 0
+} */
 
-
+   
  localStorage.setItem('score', JSON.stringify(score));
